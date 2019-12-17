13afe315963b69db480e26d159a33224d984a825
EmilyAbrahart <emilyabrahart@gmail.com>
2019-12-17 22:10:45 +0000

    chore(setup): installed jest and supertest, created and ran basic test to check GET functionality of the API, added extra eslint config file to disable some rules for test files only.

--- __tests__/sample.test.js
+++ __tests__/sample.test.js
@@ -1,5 +1,19 @@
-describe("Sample Test", () => {
-  it("should test that true === true", () => {
+const request = require('supertest');
+const server = require('../api/server');
+
+describe('/ [GET]', () => {
+  it('returns a 200 status', () => {
+    return request(server)
+      .get('/')
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.status).toEqual(200);
+      });
+  });
+});
+
+describe('Sample Test', () => {
+  it('should test that true === true', () => {
     expect(true).toBe(true);
   });
 });
