31aa8fa351b51f9f34f83eb0f122a80b98de0d45
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-12-18 16:14:12 +0100

    added seed for salary review

--- database/seeds/06-add-salary-reviews.js
+++ database/seeds/06-add-salary-reviews.js
@@ -1,10 +1,45 @@
 exports.seed = function(knex) {
- 
   // Inserts seed entries
   return knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+    {
+      user_id: 1,
+      company_id: 1,
+      description: 'Accenture Programmer',
+      salary: 95000,
+      currency: 'USD',
+      is_accepting_questions: 0,
+    },
+    {
+      user_id: 2,
+      company_id: 3,
+      description: 'Software Engineer',
+      salary: 98000,
+      currency: 'USD',
+      is_accepting_questions: 0,
+    },
+    {
+      user_id: 3,
+      company_id: 10,
+      description: 'Junior Developer',
+      salary: 3000000,
+      currency: 'NGN',
+      is_accepting_questions: 1,
+    },
+    {
+      user_id: 4,
+      company_id: 1,
+      description: 'Accenture Technical Specialist',
+      salary: 85000,
+      currency: 'USD',
+      is_accepting_questions: 1,
+    },
+    {
+      user_id: 5,
+      company_id: 13,
+      description: 'Software Engineer',
+      salary: '4800000',
+      currency: 'NGN',
+      is_accepting_questions: 0,
+    },
   ]);
-
 };
