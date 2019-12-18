5630ec15ce8a6537f3bc27867db4218a8a826d4c
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-12-18 14:15:58 +0100

    added interests

--- database/seeds/03-add-interests.js
+++ database/seeds/03-add-interests.js
@@ -1,10 +1,25 @@
 exports.seed = function(knex) {
- 
   // Inserts seed entries
-  return knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  return knex('interests').insert([
+    { user_id: 1, interests: 'Front End' },
+    { user_id: 1, interests: 'Back End' },
+    { user_id: 1, interests: 'Full Stack' },
+    { user_id: 1, interests: 'Data Science' },
+    { user_id: 1, interests: 'Machine Learning' },
+    { user_id: 2, interests: 'Front End' },
+    { user_id: 2, interests: 'Back End' },
+    { user_id: 2, interests: 'Full Stack' },
+    { user_id: 2, interests: 'User Experience' },
+    { user_id: 2, interests: 'Data Science' },
+    { user_id: 3, interests: 'Front End' },
+    { user_id: 3, interests: 'Back End' },
+    { user_id: 3, interests: 'Full Stack' },
+    { user_id: 3, interests: 'Product Manager' },
+    { user_id: 4, interests: 'Front End' },
+    { user_id: 5, interests: 'Front End' },
+    { user_id: 5, interests: 'Back End' },
+    { user_id: 5, interests: 'Full Stack' },
+    { user_id: 5, interests: 'Mobile Development' },
+    { user_id: 5, interests: 'Data Science' },
   ]);
-
 };
