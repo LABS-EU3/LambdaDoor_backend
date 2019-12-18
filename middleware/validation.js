a71af9148ea2ea9b5622bfb42cab362a21244251
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-12-18 12:37:08 +0000

    feat(editProfile): fixed issue with return statement in middleware

--- middleware/validation.js
+++ middleware/validation.js
@@ -4,12 +4,11 @@ const userExists = async (req, res, next) => {
   const { id } = req.params;
   try {
     const user = await Users.findById(id);
-    if (user) {
-      req.user = user;
-      next();
-    } else {
+    if (!user) {
       return res.status(401).json({ error: 'User does not exist' });
     }
+    req.user = user;
+    return next();
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
