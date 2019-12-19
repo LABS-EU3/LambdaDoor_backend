1860a07259c6fdd0e1fa3f29001ef5ee64f8b3c8
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-12-19 12:19:17 +0000

    fig(bug): small change to knexfile

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   test: {
     client: 'pg',
-    connection: process.env.PG_TEST,
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './database/migrations',
     },
