d794f80a242a0d64b641a82ce5074ce3677904d1
Lisa Wilton <mishmao14@gmail.com>
2020-01-21 14:19:13 +0000

    join company table to retrieve company name with reviews

--- reviews/salaryReviews/salaryReviewsModel.js
+++ reviews/salaryReviews/salaryReviewsModel.js
@@ -1,7 +1,20 @@
 const db = require('../../database/db-config');
 
 function getReviews() {
-  return db('salary_reviews');
+  return db
+    .select(
+      'sr.id',
+      'sr.user_id',
+      'sr.company_id',
+      'sr.text',
+      'c.name',
+      'sr.description',
+      'sr.salary',
+      'sr.currency',
+      'sr.is_accepting_questions'
+    )
+    .from('salary_reviews as sr')
+    .leftJoin('companies as c', 'sr.company_id', 'c.id');
 }
 
 module.exports = {
