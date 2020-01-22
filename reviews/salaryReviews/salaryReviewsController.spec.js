ca0342088cc05dd7824fdd04227f37d2584368ca
Lisa Wilton <mishmao14@gmail.com>
2020-01-22 13:45:37 +0000

    bug-fix: found bug in test script

--- reviews/salaryReviews/salaryReviewsController.spec.js
+++ reviews/salaryReviews/salaryReviewsController.spec.js
@@ -35,13 +35,15 @@ const testReview3 = {
   currency: 'NGN',
   is_accepting_questions: 1,
   is_current_employee: 1,
+  is_anonymous: 1,
+  job_title: 'Junior Dev',
 };
 
 beforeAll(async () => {
   await db.raw('TRUNCATE users RESTART IDENTITY CASCADE');
   await db.raw('TRUNCATE companies RESTART IDENTITY CASCADE');
-  await db.raw('TRUNCATE company_reviews RESTART IDENTITY CASCADE');
   await db.raw('TRUNCATE interests RESTART IDENTITY CASCADE');
+  await db.raw('TRUNCATE salary_reviews RESTART IDENTITY CASCADE');
 
   await db('users').insert(testUser3);
   await db('companies').insert(testCompany3);
@@ -49,7 +51,7 @@ beforeAll(async () => {
   await db('salary_reviews').insert(testReview3);
 });
 
-describe('companyReviews router', () => {
+describe('salaryReviews router', () => {
   describe('GET /user/:id', () => {
     test('returns a 200 response if user exists', async () => {
       await request(server)
@@ -71,79 +73,7 @@ describe('companyReviews router', () => {
         .send({ salary: 2800000 })
         .expect(200);
 
-      expect(response.body.salary).toEqual('2800000');
-    });
-
-    test("returns a 400 if salary review doesn't exist", async () => {
-      await request(server)
-        .patch('/salaryreviews/2')
-        .expect(400);
-    });
-  });
-
-  describe('DELETE /:id', () => {
-    test('returns a 204 response if salary review exists', async () => {
-      await request(server)
-        .delete('/salaryreviews/1')
-        .expect(204);
-    });
-
-    test("returns a 400 if salary review doesn't exist", async () => {
-      await request(server)
-        .delete('/salaryreviews/2')
-        .expect(400);
-    });
-  });
-
-  describe('POST /', () => {
-    test('returns a 201 response if user created salary review succesfully', async () => {
-      const response = await request(server)
-        .post('/salaryreviews/')
-        .send(testReview3)
-        .expect(201);
-
-      expect(response.body.description).toBe('Junior Developer');
-    });
-  });
-
-  describe('GET /salaryreviews/reviews/:id', () => {
-    test('returns a 200 response if salary review was fetched succesfully', async () => {
-      await request(server)
-        .get('/salaryreviews/reviews/1')
-        .expect(200);
-    });
-  });
-});
-
-afterAll(async () => {
-  await db.raw('TRUNCATE users RESTART IDENTITY CASCADE');
-  await db.raw('TRUNCATE companies RESTART IDENTITY CASCADE');
-  await db.raw('TRUNCATE company_reviews RESTART IDENTITY CASCADE');
-});
-
-describe('companyReviews router', () => {
-  describe('GET /user/:id', () => {
-    test('returns a 200 response if user exists', async () => {
-      await request(server)
-        .get('/salaryreviews/user/1')
-        .expect(200);
-    });
-
-    test("returns a 400 if user doesn't exist", async () => {
-      await request(server)
-        .get('/salaryreviews/user/2')
-        .expect(400);
-    });
-  });
-
-  describe('PATCH /:id', () => {
-    test('returns a 200 response if salary review exists', async () => {
-      const response = await request(server)
-        .patch('/salaryreviews/1')
-        .send({ salary: 2800000 })
-        .expect(200);
-
-      expect(response.body.salary).toEqual('2800000');
+      expect(response.body.salary).toEqual(2800000);
     });
 
     test("returns a 400 if salary review doesn't exist", async () => {
