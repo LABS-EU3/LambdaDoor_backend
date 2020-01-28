1eb4f586c5c81e328556d329c05b041106f759ce
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2020-01-28 12:53:07 +0100

    updated migration file

--- database/migrations/20200127095749_update-interview-process-table.js
+++ database/migrations/20200127095749_update-interview-process-table.js
@@ -2,11 +2,22 @@ exports.up = function(knex) {
   return knex.schema.table('interview_process_reviews', table => {
     table.boolean('is_accepting_questions');
     table.boolean('is_current_employee');
+    table.string('job_title');
+    table
+      .integer('interest_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('interests')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
   });
 };
 
 exports.down = function(knex) {
   return knex.schema.table('interview_process_reviews', table => {
+    table.integer('interest_id');
+    table.string('job_title');
     table.boolean('is_current_employee');
     table.boolean('is_accepting_questions');
   });
