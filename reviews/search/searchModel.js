95d411f022488a61ed408c73132252f3595fdec1
Lisa Wilton <mishmao14@gmail.com>
2020-01-28 16:59:30 +0000

    interview-search functional by location and interest

--- reviews/search/searchModel.js
+++ reviews/search/searchModel.js
@@ -14,12 +14,21 @@ function companySearch(searchTerms) {
     );
 }
 
-// function interviewSearch(searchTerms) {
-//     return db
-//     .select('*')
-//     .from('interview_process_reviews as i')
-//     .leftJoin('')
-// }
+function interviewSearch(searchTerms) {
+  return db
+    .select('*', 'i.interest', 'c.location')
+    .from('interview_process_reviews as ipr')
+    .leftJoin('interests as i', 'ipr.interest_id', 'i.id')
+    .leftJoin('companies as c', 'ipr.company_id', 'c.id')
+    .whereRaw(
+      'LOWER(i.interest) LIKE ?',
+      `%${searchTerms.search_query.toLowerCase()}%`
+    )
+    .orWhereRaw(
+      'LOWER(c.location) LIKE ?',
+      `%${searchTerms.search_query.toLowerCase()}%`
+    );
+}
 
 module.exports = {
   companySearch,
