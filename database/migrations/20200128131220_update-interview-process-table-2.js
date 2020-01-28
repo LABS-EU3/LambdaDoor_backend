be11c95a1f5cdae3d5b5aef06c0e09e0d11c38ad
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2020-01-28 13:19:24 +0100

    fixed bug

--- database/migrations/20200128131220_update-interview-process-table-2.js
+++ database/migrations/20200128131220_update-interview-process-table-2.js
@@ -14,7 +14,7 @@ exports.up = function(knex) {
 
 exports.down = function(knex) {
   return knex.schema.table('interview_process_reviews', table => {
-    table.integer('interest_id');
-    table.string('job_title');
+    table.dropColumn('interest_id');
+    table.dropColumn('job_title');
   });
 };
