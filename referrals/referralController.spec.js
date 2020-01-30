df3b528f9c0a039ef07adc68fef159387cc14bdc
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2020-01-30 14:56:18 +0100

    updated test for nodemailer

--- referrals/referralController.spec.js
+++ referrals/referralController.spec.js
@@ -1,17 +1,17 @@
 const request = require('supertest');
 const server = require('../api/server');
-const transporter = require('../utils/nodemailer');
+const referrals = require('./referralController');
 
 const testReferral = {
   name: 'Chioma Nkem-Eze',
-  senderEmail: 'fegaeze@gmail.com',
-  recipientEmail: 'fegaeze@gmail.com',
+  senderEmail: 'fake@gmail.com',
+  recipientEmail: 'gmail@yahoo.com',
   description: 'Thanks for your review, please refer me!',
 };
 
 describe('referralRouter', () => {
   describe('POST /referral', () => {
-    jest.spyOn(transporter, 'transporter').mockResolvedValue({ success: true });
+    jest.spyOn(referrals, 'sendMail').mockImplementation();
     test('returns a 200 response if request sent succesfully', async () => {
       const response = await request(server)
         .post('/referral')
